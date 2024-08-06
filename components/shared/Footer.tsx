47c5dda8b2ccdc7056c1bee339e3b1fae2d6358b
shelton <damwcs@gmail.com>
2024-08-06 13:39:20 +1000

    complete footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="border-t">
+      <div className="flex-between wrapper flex-col text-center sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38}></Image>
+        </Link>
+
+        <p>2023 Evently. All Rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,5 +1,27 @@
+'use client';
+import Link from 'next/link';
+import { headerLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
+
 const NavItems = () => {
-  return <div>NavItems</div>;
+  const pathname = usePathname();
+
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        console.log(pathname);
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={link.route}
+            className={`${isActive && 'text-primary-500'} flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavItems;
