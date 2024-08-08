46fb47468c8424a7394bc197d5b34818ddb79f5e
shelton <damwcs@gmail.com>
2024-08-08 14:15:15 +1000

    test

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -49,9 +49,10 @@ export async function POST(req: Request) {
   }
 
   //从这里开始的webhook事件处理代码
-  // Get the ID and type
-  // const { id } = evt.data;
+  const { id } = evt.data;
   const eventType = evt.type;
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log('Webhook body:', body);
 
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
