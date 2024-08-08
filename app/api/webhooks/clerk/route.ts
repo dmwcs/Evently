1aa09f9d4dffc30b1a2965513b28bc057ac1dd61
shelton <damwcs@gmail.com>
2024-08-08 15:05:51 +1000

    test

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -54,7 +54,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id = '111', email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id!,
