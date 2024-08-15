22479c3496bc08cd4dfce55d894ec7e4450d2d85
shelton <damwcs@gmail.com>
2024-08-15 16:32:50 +1000

    finishing collection

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import Collection from '@/components/shared/Collection';
+import { getAllEvents } from '@/lib/actions/event.actions';
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({ query: '', category: '', page: 1, limit: 6 });
+  console.log(events);
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -33,6 +38,15 @@ export default function Home() {
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">serch categoryfilter</div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={3}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
